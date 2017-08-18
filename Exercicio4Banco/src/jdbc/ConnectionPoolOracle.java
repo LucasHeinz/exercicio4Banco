dcbc877a69e8ddf07d0e437bb01a4b72a7722551
Noturno <Noturno@192.168.7.189>
2017-08-17 21:27:21 -0300

    INCRÍVEL MENSAGEM DE COMMIT :D

---
+++ Exercicio4Banco/src/jdbc/ConnectionPoolOracle.java
@@ -0,0 +1,26 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.PooledConnection;
+
+import oracle.jdbc.pool.OracleConnectionPoolDataSource;
+
+public class ConnectionPoolOracle {
+
+	private PooledConnection pool;
+
+	public ConnectionPoolOracle() throws SQLException {
+		OracleConnectionPoolDataSource ocpds = new OracleConnectionPoolDataSource();
+		ocpds.setURL("jdbc:oracle:thin:@localhost:1521:xe");
+		ocpds.setUser("exercicio4");
+		ocpds.setPassword("exercicio4");
+		pool = ocpds.getPooledConnection();
+	}
+
+	public Connection getConnection() throws SQLException {
+		return pool.getConnection();
+	}
+
+}
